4e0b6c2f251cfa7af6ca8b21f01d5c8353d43216
Daniel Murphy <danielmurphy02@gmail.com>
2013-02-21 19:33:09 -0800

    refactoring

--- src/sketch.ino
+++ src/sketch.ino
@@ -19,6 +19,10 @@ public:
     setColors(r, g, b);
   }
 
+  void off(){
+    setColors(0,0,0);
+  }
+
 private:
   void setColors(int r, int g, int b) {
     red = r;
@@ -28,8 +32,9 @@ private:
 };
 
 class ShiftBriteShield {
-  int dataPin, latchPin, enablePin, clockPin;
 public:
+  int dataPin, latchPin, enablePin, clockPin;
+  LED lights[NUM_LEDS];
 
   ShiftBriteShield (int dp, int lp, int ep, int cp){
     dataPin = dp;
@@ -46,9 +51,16 @@ public:
     digitalWrite(enablePin, LOW);
   }
 
-  void draw(LED leds[NUM_LEDS]) {
+  void clear() {
     for (int i = 0; i < NUM_LEDS; i++) {
-      sendPacket(B00, leds[i]);
+      lights[i].off();
+    }
+    draw();
+  }
+
+  void draw() {
+    for (int i = 0; i < NUM_LEDS; i++) {
+      sendPacket(B00, lights[i]);
     }
 
     delayMicroseconds(15);
@@ -56,7 +68,7 @@ public:
     delayMicroseconds(15);
     digitalWrite(latchPin,LOW);
 
-    for (int i = 0; i < NUM_LEDS; i++) sendPacket(B01, leds[i]);
+    for (int i = 0; i < NUM_LEDS; i++) sendPacket(B01, lights[i]);
       delayMicroseconds(15);
       digitalWrite(latchPin,HIGH); // latch data into registers
       delayMicroseconds(15);
@@ -93,27 +105,42 @@ private:
 
 ShiftBriteShield shield (DATA_PIN, LATCH_PIN, ENABLE_PIN, CLOCK_PIN);
 
-LED lights[NUM_LEDS];
 
 void setup() {
-  LED redLed(255, 0, 0);
-  LED greenLed(0, 255, 0);
-  LED blueLed(0, 0, 255);
 
-  LED colors[3];
-  colors[0] = redLed;
-  colors[1] = greenLed;
-  colors[2] = blueLed;
+  // LED colors[3];
+  // colors[0] = redLed;
+  // colors[1] = greenLed;
+  // colors[2] = blueLed;
 
-  for (int i = 0; i < NUM_LEDS; i++) {
-    lights[i] = colors[i % 3];
-  }
 
-  shield.draw(lights);
+  // // for (int i = 0; i < NUM_LEDS; i++) {
+  // //   lights[i] = colors[i % 3];
+  // // }
+
+  // // shield.draw(lights);
+  // // delay(500);
+
 }
 
 
 void loop() {
+  LED redLed(255, 0, 0);
+  LED greenLed(0, 255, 0);
+  LED blueLed(0, 0, 255);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    shield.clear();
+    shield.lights[i] = greenLed;
+    shield.draw();
+    delay(200);
+  }
+
+  for (int i = NUM_LEDS - 1; i >= 0; i--) {
+    shield.clear();
+    shield.lights[i] = greenLed;
+    shield.draw();
+    delay(200);
+  }
   // Serial.println(red[0]);
 
   // shield.draw([0][])
